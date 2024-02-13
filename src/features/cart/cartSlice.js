c7af2e13125f783b8ebf70fecbabf750f85c10f1
Zahidul-Turja <zahidulturja@gmail.com>
2024-02-13 10:13:29 +0600

    delete cart item

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -51,10 +51,17 @@ export const {
   clearCart,
 } = cartSlice.actions;
 
+export const getCart = (state) => state.cart.cart;
+
 export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
 
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
 
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
+
 export default cartSlice.reducer;
+
+// ! An important function --> `reselect()`
